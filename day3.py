10b4434c2fc4aaa5979a4f12dea5c68ddb343c6f
carahappe <argus.cara@gmail.com>
2024-12-03 15:30:14 -0600

    day 3 part 1 correct

--- day3.py
+++ day3.py
@@ -1,7 +1,8 @@
 with open('./day3_input.txt', 'r') as file:
     content = file.read()
     import re
-    mul_instructions = re.findall("(mul\([1-9]*,[1-9]*\))", content)
+    mul_instructions = re.findall("(mul\([0-9]*,[0-9]*\))", content)
+    print(mul_instructions)
     total = 0
     clean_instructions = {}
     for sets in mul_instructions:
