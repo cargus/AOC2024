e0ccc6235398fc6ad9bd5f64642aa75c8c0b0947
carahappe <argus.cara@gmail.com>
2024-12-04 14:35:53 -0600

    day 4 part 1, not working, number of Ms is wrong

--- day4.py
+++ day4.py
@@ -1,12 +1,12 @@
 #check Second letter and determine direction
 def check_letter (x_d,y_d, letter, list_out, compass):
-    if -1< x_d < int(n_columns) and -1 < y_d < int(n_rows) and content[int(x_d)] [int(y_d)] == letter:
+    if -1< x_d < int(n_columns-1) and -1 < y_d < int(n_rows-1) and content[int(x_d)] [int(y_d)] == letter:
         list_out[x_d,y_d] = compass
         return list_out
 
 #check letters after direction determined
 def check_letter_direction (X_D, Y_D, letter, list_out, compass):
-    if -1 < X_D < int(n_columns) and -1 < Y_D < int(n_rows) and direction == compass and content[int(X_D)][int(Y_D)] == letter:
+    if -1 < X_D < int(n_columns-1) and -1 < Y_D < int(n_rows-1) and direction == compass and content[int(X_D)][int(Y_D)] == letter:
         list_out[X_D, Y_D] = direction
 
 #open input file
@@ -31,9 +31,10 @@ for lindex, line in enumerate(content):
                 coordinates = lindex, rindex
                 X_list.append(coordinates)
 print ("the number of Xs is " + str(len(X_list)))
+print (X_list)
 
 #get list of Ms by the Xs and determine direction
-for X , Y in X_list:
+for Y , X in X_list:
 #define all coordinates to look at
     X_N = X
     Y_N = Y-1
@@ -61,15 +62,16 @@ for X , Y in X_list:
     check_letter(X_SW,Y_SW,"M", M_list,"SW")
     check_letter(X_W,Y_W,"M", M_list,"W")
     check_letter(X_NW,Y_NW,"M", M_list,"NW")
-
+print ("the number of Ms is " + str(len(M_list)))
+print (M_list)
 
 #get list of As by the Ms and following direction
 for coordinates , direction in M_list.items():
 #clean up X and Y coordinates from the dictionary list
     X_Y = str(coordinates).split(", ")
-    X = X_Y[0].lstrip('(')
+    X = X_Y[1].rstrip(')')
     X = int(X)
-    Y = X_Y[1].rstrip(')')
+    Y = X_Y[0].lstrip('(')
     Y = int(Y)
 #define all coordinates to look at
     X_N = X
@@ -102,9 +104,9 @@ for coordinates , direction in M_list.items():
 for coordinates , direction in A_list.items():
 #clean up X and Y coordinates from the dictionary list
     X_Y = str(coordinates).split(", ")
-    X = X_Y[0].lstrip('(')
+    X = X_Y[1].rstrip(')')
     X = int(X)
-    Y = X_Y[1].rstrip(')')
+    Y = X_Y[0].lstrip('(')
     Y = int(Y)
 #define all coordinates to look at
     X_N = X
