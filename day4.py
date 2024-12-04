f0b20379640acc0fd22bf5377226794d922baacd
carahappe <argus.cara@gmail.com>
2024-12-04 11:59:49 -0600

    day 4 part 1, first pass, not working

--- day4.py
+++ day4.py
@@ -1,2 +1,151 @@
-with open('./day4_input.txt', 'r') as file:
+with open('inputs/day4_input.txt', 'r') as file:
     content = file.read().splitlines()
+    print (content[0][1])
+    #setup lists/ dictionaries for letter coordinates
+    X_list = []
+    M_list = {}
+    A_list = {}
+    count = 0
+
+    #get size of the word search
+    n_rows = len(content)
+    column_count = [len(string) for string in content]
+    n_columns = column_count[0]
+
+    #find coordinates of all X's in the word search
+    for lindex, line in enumerate(content):
+        for rindex, character in enumerate (line):
+                if character == "X":
+                    coordinates = lindex, rindex
+                    X_list.append(coordinates)
+
+#get list of Ms by the Xs and determine direction
+for X , Y in X_list:
+    X_N = X
+    Y_N = Y-1
+    X_NE = X+1
+    Y_NE = Y-1
+    X_E = X+1
+    Y_E = Y
+    X_SE = X+1
+    Y_SE = Y+1
+    X_S = X
+    Y_S = Y+1
+    X_SW = X-1
+    Y_SW = Y+1
+    X_W = X-1
+    Y_W = Y
+    X_NW = X-1
+    Y_NW = Y-1
+    #check North
+    if -1< X_N < int(n_columns) and -1 < Y_N < int(n_rows) and content[int(X_N)] [int(Y_N)] == "M":
+        M_list[X_N,Y_N] = "N"
+    #check North East
+    if -1< X_NE < int(n_columns) and -1 < Y_NE < int(n_rows) and content[int(X_NE)] [int(Y_NE)] == "M":
+        M_list[X_NE,Y_NE] = "NE"
+    #check East
+    if -1< X_E < int(n_columns) and -1 < Y_E < int(n_rows) and content[int(X_E)] [int(Y_E)] == "M":
+        M_list[X_E, Y_E] = "E"
+    # check South East
+    if -1< X_SE < int(n_columns) and -1 < Y_SE < int(n_rows) and content[int(X_SE)] [int(Y_SE)] == "M":
+        M_list[X_SE, Y_SE] = "SE"
+    # check South
+    if -1< X_S < int(n_columns) and -1 < Y_S < int(n_rows) and content[int(X_S)] [int(Y_S)] == "M":
+        M_list[X_S, Y_S] = "S"
+    # check South West
+    if -1< X_SW < int(n_columns) and -1 < Y_SW < int(n_rows) and content[int(X_SW)] [int(Y_SW)] == "M":
+        M_list[X_SW, Y_SW] = "SW"
+    # check West
+    if -1< X_W < int(n_columns) and -1 < Y_W < int(n_rows) and content[int(X_W)] [int(Y_W)] == "M":
+        M_list[X_W,Y_W] = "W"
+#get list of As by the Ms and following direction
+for coordinates , direction in M_list.items():
+    X_Y = str(coordinates).split(", ")
+    X = X_Y[0].lstrip('(')
+    X = int(X)
+    Y = X_Y[1].rstrip(')')
+    Y = int(Y)
+    X_N = X
+    Y_N = Y-1
+    X_NE = X+1
+    Y_NE = Y-1
+    X_E = X+1
+    Y_E = Y
+    X_SE = X+1
+    Y_SE = Y+1
+    X_S = X
+    Y_S = Y+1
+    X_SW = X-1
+    Y_SW = Y+1
+    X_W = X-1
+    Y_W = Y
+    X_NW = X-1
+    Y_NW = Y-1
+#check North
+    if -1< X_N < int(n_columns) and -1 < Y_N < int(n_rows) and direction == "N" and content[int(X_N)] [int(Y_N)] == "A":
+        A_list[X_N,Y_N] = "N"
+    #check North East
+    if -1< X_NE < int(n_columns) and -1 < Y_NE < int(n_rows) and direction == "NE" and content[int(X_NE)] [int(Y_NE)] == "A":
+        A_list[X_NE,Y_NE] = "NE"
+    #check East
+    if -1< X_E < int(n_columns) and -1 < Y_E < int(n_rows) and direction =="E" and content[int(X_E)] [int(Y_E)] == "A":
+        A_list[X_E, Y_E] = "E"
+    # check South East
+    if -1< X_SE < int(n_columns) and -1 < Y_SE < int(n_rows) and direction =="SE" and content[int(X_SE)] [int(Y_SE)] == "A":
+        A_list[X_SE, Y_SE] = "SE"
+    # check South
+    if -1< X_S < int(n_columns) and -1 < Y_S < int(n_rows) and direction == "S" and content[int(X_S)] [int(Y_S)] == "A":
+        A_list[X_S, Y_S] = "S"
+    # check South West
+    if -1< X_SW < int(n_columns) and -1 < Y_SW < int(n_rows) and direction == "SW" and content[int(X_SW)] [int(Y_SW)] == "A":
+        A_list[X_SW, Y_SW] = "SW"
+    # check West
+    if -1< X_W < int(n_columns) and -1 < Y_W < int(n_rows) and direction =="W" and content[int(X_W)] [int(Y_W)] == "A":
+        A_list[X_W,Y_W] = "W"
+
+#get count of Ss by the As and following direction
+for coordinates , direction in A_list.items():
+    X_Y = str(coordinates).split(", ")
+    X = X_Y[0].lstrip('(')
+    X = int(X)
+    Y = X_Y[1].rstrip(')')
+    Y = int(Y)
+    X_N = X
+    Y_N = Y-1
+    X_NE = X+1
+    Y_NE = Y-1
+    X_E = X+1
+    Y_E = Y
+    X_SE = X+1
+    Y_SE = Y+1
+    X_S = X
+    Y_S = Y+1
+    X_SW = X-1
+    Y_SW = Y+1
+    X_W = X-1
+    Y_W = Y
+    X_NW = X-1
+    Y_NW = Y-1
+#check North
+    if -1< X_N < int(n_columns) and -1 < Y_N < int(n_rows) and direction == "N" and content[int(X_N)] [int(Y_N)] == "S":
+        count +=1
+    #check North East
+    if -1< X_NE < int(n_columns) and -1 < Y_NE < int(n_rows) and direction == "NE" and content[int(X_NE)] [int(Y_NE)] == "S":
+        count +=1
+    #check East
+    if -1< X_E < int(n_columns) and -1 < Y_E < int(n_rows) and direction =="E" and content[int(X_E)] [int(Y_E)] == "S":
+        count +=1
+    # check South East
+    if -1< X_SE < int(n_columns) and -1 < Y_SE < int(n_rows) and direction =="SE" and content[int(X_SE)] [int(Y_SE)] == "S":
+        count +=1
+    # check South
+    if -1< X_S < int(n_columns) and -1 < Y_S < int(n_rows) and direction == "S" and content[int(X_S)] [int(Y_S)] == "S":
+        count +=1
+    # check South West
+    if -1< X_SW < int(n_columns) and -1 < Y_SW < int(n_rows) and direction == "SW" and content[int(X_SW)] [int(Y_SW)] == "S":
+        count +=1
+    # check West
+    if -1< X_W < int(n_columns) and -1 < Y_W < int(n_rows) and direction =="W" and content[int(X_W)] [int(Y_W)] == "S":
+        count +=1
+
+print(count)
