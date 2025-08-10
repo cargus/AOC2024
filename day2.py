990eeb3c34bd2e48d523e22162c925959369fcab
carahappe <argus.cara@gmail.com>
2025-08-10 14:28:05 -0600

    Refactoring of Day 2 for classwork.

--- day2.py
+++ day2.py
@@ -7,81 +7,70 @@ correct trend.
 """
 
 """
+name: check_report_consistency
+parameters: readings - the list of readings to be checked
+return: true if report consistently incrementing, false if not
+purpose: check if the list of numbers passed in is consistently incrementing by no more than 3
+"""
+def check_report_consistency(readings:[int])-> bool:
+    i=0
+    # length has 1 subtracted to prevent an overflow error due to using i + 1 for the difference
+    while i < len(readings) - 1:
+        difference = int(readings[i + 1]) - int(readings[i])
+        if 0 < difference < 4:
+            i += 1
+            continue
+        else:
+            return False
+    return True
+
+"""
 name: is_safe
-parameters: readings, tolerance
+parameters: readings - the list of readings to be checked
+            tolerance - True means a number has been removed, tolerance has been applied to readings
 return: Is string safe (Boolean)
 purpose: Check if input string is consistently increasing or decreasing by 3 or less.
 """
-def is_safe(readings: list[str], tolerance: int = 0) -> bool:
-    #get the number of numbers in the list
-    #The subtract one is because of using i +1 for the difference, without you get an overflow error.
-    length = len(readings) - 1
-    # set the default levels for variables
-    i = 0
-    direction = ""
-    difference = int(readings[i]) - int(readings[i + 1])
-    status = ""
+def is_safe(readings: list[int], tolerance: bool = False) -> bool:
+    #finding the difference of the first two integers to determine the expected direction
+    difference = readings[1] - readings[0]
 
-    #check for a positive or negative difference and assign direction, if zero, this fails
+    #reverse list if descending
     if difference < 0:
-        direction = "down"
-    if difference > 0:
-        direction = "up"
+        readings.reverse()
 
-    #check the rest of the numbers follow the down direction by no more than 3
-    while i < length and direction == "down":
-        difference = int(readings[i]) - int(readings[i + 1])
-        if -4 < difference < 0:
-            status = "pass"
-        else:
-            status = ""
-            break
-        i += 1
-
-    #check the rest of the numbers follow the up direction by no more than 3
-    while i < length and direction == "up":
-        difference = int(readings[i]) - int(readings[i + 1])
-        if 0 < difference < 4:
-            status = "pass"
-        else:
-            status = ""
-            break
-        i += 1
-
-    #return true if all numbers pass the rules
-    if status == "pass":
+    #Checks for report consistently incrementing and returns True if it is and then ends function with True
+    if check_report_consistency(readings):
         return True
 
     #logic handling for one "bad reading"
-    elif tolerance == 0:
-        #remove the "bad reading" and create a new list without that number, determine if it now passes
+    if not tolerance:
+        #create new list without one reading, determine if it now passes for each index
         for counter in range(len(readings)):
             new_list = list(readings)
             del new_list[counter]
-            if is_safe(new_list, 1):
-                print(readings)
-                print("true")
+            if is_safe(new_list, True):
                 return True
-        #returns false if the new list breaks any of the rules
-        return False
-    #returns false if one measurement has already been removed
+    #returns false if one measurement being removed does not result in a valid list
     return False
 
 def __main__():
-    # open input file and read in lines
     with open('inputs/day2_input.txt', 'r') as file:
         content = file.read().splitlines()
-    # set # of safe reports to zero
-    safe_reports = 0
 
-    #handle logic for each line
+    # tracking of number of safe reports
+    safe_reports = 0
     for line in content:
-        # split string on spaces
         line_input = line.split(" ")
-        #run the is safe function and if returns true, increase the safe reports count by 1
-        if is_safe(line_input, 0):
+
+        #convert all values to ints to allow numeric comparisons
+        reading = []
+        for num in line_input:
+            reading.append(int(num))
+
+        if is_safe(reading):
             safe_reports +=1
-    # output the number of safe reports in the report
+
     print(f"The number of safe reports is {safe_reports}")
 
 __main__()
